2595be81e25acc509b504abb49c517311482a175
Logan Campbell <colin@logaan.net>
2014-12-17 16:16:32 +1100

    lein new hat

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hat
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hat/core_test.clj
@@ -0,0 +1,7 @@
+(ns hat.core-test
+  (:require [clojure.test :refer :all]
+            [hat.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
