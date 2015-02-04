61ca4b45e207cacb2208cd2e09f1294765cb99cc
Logan Campbell <colin@logaan.net>
2015-02-04 17:22:41 +1100

    removed datomic

--- project.clj
+++ project.clj
@@ -5,10 +5,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.datomic/datomic-pro "0.9.4815.12"] 
                  [org.clojure/data.codec "0.1.0"]
                  [bidi "1.10.4"]
                  [http-kit "2.1.16"]
                  [liberator "0.11.1"]]
-  :test-matcher #".*test"
-  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"}})
+  :test-matcher #".*test")
